b681f89947d6abe16f24ff8c23764797054c50bb
XakimAA <malutok@mail.ru>
2018-08-17 14:02:37 +0500

    html-loader для html-файлов to-string-loader и css-loader для css-файлов url-loader для всех изображений меньше 10кб mini-css-extract-plugin

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import small from './image/8kb.gif'
+import big from './image/38kb.gif'
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,59 @@
-var path = require('path');
- 
+var path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
   module : {
-    rules: [ //объекты с правилами загрузки модулей
+    rules: [ 
         { 
             test:/\.js$/,
             loader: 'babel-loader'
-        }
+        },
+        {
+            test: /\.html$/,
+            loader: "html-loader"
+        },  
+        {
+            test: /\.scss$/,
+            loaders: [
+                'to-string',
+                'css',
+                'sass'
+            ]
+        },
+        {
+            test: /\.(png|jpg|gif)$/i,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: 10240
+                }
+              }
+            ]
+        },
+        {
+            test: /\.css$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  publicPath: '../'
+                }
+              },
+              "css-loader"
+            ]
+          }
     ]
 }
 };
